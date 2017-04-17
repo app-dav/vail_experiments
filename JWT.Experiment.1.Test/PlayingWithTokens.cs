5952ae286677517ec948d43d1b150a1027c09c2a
VRI_DOMAIN\dappel <dappel@vailresorts.com>
2017-04-17 16:18:54 -0600

    refactor out the builing of token

--- JWT.Experiment.1.Test/PlayingWithTokens.cs
+++ JWT.Experiment.1.Test/PlayingWithTokens.cs
@@ -11,8 +11,7 @@ namespace JWT.Experiment._1.Test
 {
     public class PlayingWithTokens
     {
-        [Fact]
-        public void TestSelfValidate()
+        private string BuildToken()
         {
             string secretKey = "mysupersecret_secretkey!123";
             string url = "http://vailresorts.com";
@@ -40,7 +39,21 @@ namespace JWT.Experiment._1.Test
 
             var token = tokenHandler.CreateToken(descr);
 
-            var stringyToken = tokenHandler.WriteToken(token);
+            return tokenHandler.WriteToken(token);
+        }
+
+        [Fact]
+    public void TestSelfValidate()
+    {
+            string secretKey = "mysupersecret_secretkey!123";
+            string url = "http://vailresorts.com";
+
+            var key = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var now = DateTime.Now;
+            var issuer = "vailResorts";
+            var stringyToken = BuildToken();
 
             Assert.NotNull(stringyToken);
 
@@ -48,12 +61,11 @@ namespace JWT.Experiment._1.Test
             {
                 AudienceValidator = (aud, tkn, vp) => { return aud.Contains(url); },
                 IssuerSigningKey = key,
-              IssuerValidator = (iss, tkn, vp) => { return issuer;  }
+                IssuerValidator = (iss, tkn, vp) => { return issuer; }
             };
             SecurityToken validToken = null;
             tokenHandler.ValidateToken(stringyToken, validators, out validToken);
             Assert.NotNull(validToken);
-            
         }
     }
 }
